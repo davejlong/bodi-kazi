b303a6cc7357945e32314f1fbad4492b2d825b20
David Long <dave@davejlong.com>
2014-04-08 21:44:54 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BodyKazi::Application.config.secret_key_base = '491bbe65a2e057b761ab9316f941ba166c922de8f4be08a8626df63b4fa2c3f43af7bc6e08b72ba2593e91a7a764f98f2dc951f212df79dea5b57317c1e768d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BodyKazi::Application.config.session_store :cookie_store, key: '_body_kazi_session'
